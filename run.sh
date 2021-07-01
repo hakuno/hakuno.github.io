03b6158b21f062d68f6f16349ae2f3bab1a973d4
Seiji Manoan <seiji.manoan@uello.com.br>
2021-07-01 20:54:04 -0300

    fix: the cmd

--- run.sh
+++ run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-export JEKYLL_VERSION=3.8.6
+JEKYLL_VERSION=3.8.6
+JEKYLL_PORT=80
+
+if [[ "${1}" == "build" ]] || [[ ! -d vendor ]]
+then
+  docker run --rm \
+    --volume="$PWD:/srv/jekyll" \
+    --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+    -it jekyll/jekyll:$JEKYLL_VERSION \
+    jekyll build
+fi
+
+echo -e "Should run on http://localhost:${JEKYLL_PORT}...\n---"
 
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll build
-
-docker run --rm \
-  --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-  --publish 0.0.0.0:80:4000/tcp \
+  --publish 0.0.0.0:$JEKYLL_PORT:4000/tcp \
+  --name jekyll \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve
